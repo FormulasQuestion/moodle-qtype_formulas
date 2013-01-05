035113a05bf6c890233ac8c833b0b7560daafedb
Jean-Michel Vedrine <vedrine@vedrine.org>
2013-01-06 00:31:04 +0100

    WIP upgrade of old attempts

---
+++ db/upgradelib.php
@@ -0,0 +1,156 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Upgrade library code for the formulas question type.
+ *
+ * @package    qtype_formulas
+ * @copyright  2013 Jean-Michel Vedrine
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+
+/**
+ * Class for converting attempt data for formulas questions when upgrading
+ * attempts to the new question engine.
+ *
+ * This class is used by the code in question/engine/upgrade/upgradelib.php.
+ *
+ * @copyright  2013 Jean-Michel Vedrine
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class qtype_formulas_qe2_attempt_updater extends question_qtype_attempt_updater {
+    public function right_answer() {
+        return '';
+    }
+
+    public function question_summary() {
+        $summary = $this->to_text($this->question->questiontext, $this->question->questiontextformat);
+        foreach ($this->question->options->answers as $i => $part) {
+            $answerbit = $this->to_text($part->subqtext, $part->subqtextformat);
+            if ($part->placeholder != '') {
+                $summary = str_replace('{' . $part->placeholder . '}', $answerbit, $summary);
+            } else {
+                $summary .= $answerbit;
+            }
+        }
+        return $summary;    
+    }
+
+    public function was_answered($state) {
+    echo "function was answered\n";
+        $parsedanswer = $this->parse_answer($state->answer);
+        $responses = $parsedanswer['responses'];
+        $allresponses = implode('', $parsedanswer['responses']);
+var_dump($allresponses);
+        return !empty($allresponses);
+    }
+
+    public function response_summary($state) {
+        $summary = array();
+        $parsedanswer = $this->parse_answer($state->answer);
+        $responses = $parsedanswer['responses'];
+        foreach ($this->question->options->answers as $i => $part) {
+            if ($this->has_combined_unit_field($part)) {
+                $summary [] = $responses["${i}_0"] . $responses["${i}_1"];;
+            } else {
+                foreach (range(0, $part->numbox - 1) as $j) {
+                    $summary [] = $responses["${i}_$j"];
+                }
+                if ($this->has_separate_unit_field($part)) {
+                    $summary [] = $responses["${i}_{$part->numbox}"];
+                }
+            }
+        }
+        $summary = implode('; ', $summary);
+        return $summary;
+        return '';
+    }
+
+    public function set_first_step_data_elements($state, &$data) {
+        $parsedanswer = $this->parse_answer($state->answer);
+        $data['_randomsvars_text'] = $parsedanswer['randomvars'];
+        $data['_varsglobal'] = $this->question->options->extra->varsglobal;
+    }
+
+    public function supply_missing_first_step_data(&$data) {
+        echo "function supply_missing_first_step_data\n";
+        $data['_varsglobal'] = $this->question->options->extra->varsglobal;
+
+    }
+
+    public function set_data_elements_for_step($state, &$data) {
+        $parsedanswer = $this->parse_answer($state->answer);
+        $responses = $parsedanswer['responses'];
+        foreach ($this->question->options->answers as $i => $part) {
+            if ($this->has_combined_unit_field($part)) {
+                // If part has a combined unit field we need to group answer and unit.
+                $data["${i}_"] = $responses["${i}_0"] . $responses["${i}_1"];
+            } else {
+                foreach (range(0, $part->numbox - 1) as $j) {
+                    // All coordinates of response for part.
+                    $data["${i}_$j"] = $responses["${i}_$j"];
+                }
+                if ($this->has_separate_unit_field($part)) {
+                    // If there is a separated unit field
+                    // Last response is unit.
+                    $data["${i}_{$part->numbox}"] = $responses["${i}_{$part->numbox}"];
+                }
+                
+            }
+        }
+    }
+
+    protected function parse_answer($answer) {
+        $data = array();
+        $lines = explode("\n", $answer);
+        $counter = 0;
+        $details = array();
+        while (strlen(trim($lines[$counter])) != 0) { // Read lines until an empty one is encountered.
+            $pair = explode('=', $lines[$counter], 2);
+            if (count($pair) == 2) {
+                $details[trim($pair[0])] = $pair[1];  // Skip lines without "=", if any.
+            }
+            $counter++;
+        }
+        $grading = array();
+        $responses = array();
+        foreach ($this->question->options->answers as $i => $part) {
+            $grading[$i] = array_key_exists($i, $details) ? explode(',', $details[$i]) : array(0, 0, 0, 0);
+            foreach (range(0, $part->numbox) as $j) {
+                $responses["${i}_$j"] = array_key_exists("${i}_$j", $details) ? $details["${i}_$j"] : '';
+            }
+        }
+        $subanum = intval($lines[$counter+1]);
+        $randomvars = implode("", array_slice($lines, $counter+3));
+        return array('responses' => $responses, 'subanum' => $subanum, 'grading' => $grading, 'randomvars' => trim($randomvars));
+
+    }
+
+    // Copied and adapted from question definition.
+    public function has_separate_unit_field($part) {
+        return strlen($part->postunit) != 0 && $this->has_combined_unit_field($part) == false;
+    }
+
+    // Copied and adapted from question definition.
+    public function has_combined_unit_field($part) {
+        return strlen($part->postunit) != 0 && $part->numbox == 1 && $part->answertype != 1000
+                && (strpos($part->subqtext, "{_0}{_u}") !== false || (strpos($part->subqtext, "{_0}") === false && strpos($part->subqtext, "{_u}") === false));
+    }
+}
