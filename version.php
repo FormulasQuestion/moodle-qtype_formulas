90770244fe4bcda3621a67e628b4a2404053bd70
Dominique Bauer <dominique.bauer@etsmtl.net>
2020-05-27 22:06:36 -0400

    Update version.php

--- version.php
+++ version.php
@@ -25,11 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_formulas';
-<<<<<<< Updated upstream
-$plugin->version   = 2020052200;
-=======
 $plugin->version   = 2020052700;
->>>>>>> Stashed changes
 
 $plugin->cron      = 0;
 $plugin->requires  = 2015111600;
