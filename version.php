5c10554dbeb551b2fb85ad2a4be8758447df81b5
Jakob Heinemann <jakob@jakobheinemann.de>
2022-03-12 16:57:35 +0100

    version bump

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_formulas';
-$plugin->version   = 2020061900;
+$plugin->version   = 2022031200;
 
 $plugin->cron      = 0;
 $plugin->requires  = 2015111600;
@@ -34,6 +34,6 @@ $plugin->dependencies = array(
     'qbehaviour_adaptivemultipart'     => 2014092500,
     'qtype_multichoice' => 2015111600,
 );
-$plugin->release   = '4.91 for Moodle 3.0 to 3.9';
+$plugin->release   = '4.92 for Moodle 3.0 to 3.11';
 
 $plugin->maturity  = MATURITY_STABLE;
