5ea0a5a82643052313b74b27e64b7b0d483addda
Philipp Imhof <52650214+PhilippImhof@users.noreply.github.com>
2022-11-09 14:22:21 +0100

    continued refactoring and clean up - all javascript is now ES6 - restructured the form - included Tabulator.js for instantiation table - rewritten the instantiation verification frontend - created a web service for instantiation - added on-the-fly validation of variable definitions (WIP)

--- amd/src/editform.js
+++ amd/src/editform.js
@@ -22,7 +22,9 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-import 'core/config';
+import * as Notification from 'core/notification';
+import {call as fetchMany} from 'core/ajax';
+import {TabulatorFull as Tabulator} from 'qtype_formulas/tabulator';
 
 /**
  * Default grading criterion according to plugin settings (admin)
@@ -35,6 +37,46 @@ var defaultCorrectness = '';
 var numberOfParts = 0;
 
 export const init = (defCorrectness) => {
+    Tabulator.extendModule('columnCalcs', 'calculations', {
+        'myavg': (values) => {
+            var count = 0;
+            var sum = 0;
+
+            for (let value of values) {
+                sum += parseFloat(value);
+                count++;
+            }
+
+            if (count > 0 && !isNaN(sum)) {
+                return (sum / count).toFixed(1);
+            }
+            return '';
+        },
+        'avglabel': () => {
+            return '⌀';
+        }
+    });
+    let table = new Tabulator('#varsdata_display', {
+        selectable: 1,
+        movableColumns: true,
+        pagination: 'local',
+        paginationSize: 10,
+        paginationButtonCount: 0,
+        columns: [
+            {title: '#', field: 'id', bottomCalc: 'avglabel'},
+        ],
+        langs: {
+            'default': {
+                'pagination': {
+                    'first': '⏮',
+                    'last': '⏭',
+                    'prev': '⏪',
+                    'next': '⏩'
+                }
+            }
+        },
+    });
+    table.on('rowSelected', previewQuestionWithDataset);
     defaultCorrectness = defCorrectness;
     numberOfParts = document.querySelectorAll('fieldset[id^=id_answerhdr_]').length;
 
@@ -75,6 +117,287 @@ export const init = (defCorrectness) => {
         document.getElementById(`id_correctness_simple_tol_${i}`).addEventListener(
             'change', normalizeTolerance
         );
+    }
+
+    document.getElementById('id_varsrandom').addEventListener(
+        'change', validateRandomvars
+    );
+    document.getElementById('id_varsglobal').addEventListener(
+        'change', validateGlobalvars
+    );
+    document.getElementById('id_instantiatebtn').addEventListener(
+        'click', instantiate
+    );
+};
+
+/**
+ * For proper parsing in the backend, strings must be enclosed in double quotes,
+ * but numbers must not.
+ *
+ * @param {string} value representation of a numberic, string or list (array) value
+ * @returns {string} the same value, but with quotes added, if necessary
+ */
+const quoteNonNumericValue = (value) => {
+    // Numbers must not be quoted.
+    if (!isNaN(value)) {
+        return value;
+    }
+    // For arrays, we have to check each element individually and quote, if necessary.
+    // Formulas question does not currently support nested arrays, so we don't have to deal with that.
+    if (value.startsWith('[')) {
+        let quotedElements = [];
+        // Remove leading and trailing bracket
+        value = value.substring(1, value.length - 1);
+        let elements = value.split(/\s*,\s*/);
+        for (let element of elements) {
+            quotedElements.push(quoteNonNumericValue(element));
+        }
+        return '[' + quotedElements.join(', ') + ']';
+    }
+    // Not a number and not an array, so we enclose it in double quotes.
+    return `"${value}"`;
+};
+
+/**
+ * The question text and the parts' text are stored in the editor. For some editors,
+ * we can take the content from the textarea's value attribute. For TinyMCE (and maybe others),
+ * we must use the corresponding API.
+ *
+ * @param {string} id id of the textarea
+ * @returns {string} the question or part's text
+ */
+const fetchTextFromEditor = (id) => {
+    if (typeof window.tinyMCE !== 'undefined') {
+        return window.tinyMCE.get(id).getContent();
+    }
+    return document.getElementById(id).value;
+};
+
+/**
+ * Extract data from the instantiation table (selected row) and send them to the backend,
+ * in order to have the question text and parts' text rendered for the preview.
+ *
+ * @param {object} row RowComponent from Tabulator.js
+ */
+const previewQuestionWithDataset = (row) => {
+    let data = row.getData();
+    let questionvars = '';
+    let partvars = Array(numberOfParts).fill('');
+
+    for (let varname in data) {
+        // Variables for the main question are all random or global.
+        // Also, as random variables have already been instantiated, they are not random anymore.
+        if (varname.match(/^(random|global)_/)) {
+            questionvars += varname.replace(/^(random|global)_([^*]+)\*?$/, '$2') + '=';
+            questionvars += quoteNonNumericValue(data[varname]) + ';';
+        }
+        // Variables for a question part always start with part_ + number of the part
+        if (varname.match(/^part_(\d+)_/)) {
+            // If the variable name starts with _ it should be removed, as these are
+            // answers (or otherwise reserved names, but that should not be the case)
+            if (varname.match(/^part_(\d+)__/)) {
+                continue;
+            }
+            let index = parseInt(varname.replace(/^part_(\d+)_.*$/, '$1'));
+            partvars[index] += varname.replace(/^part_(\d+)_([^*]+)\*?$/, '$2') + '=';
+            partvars[index] += quoteNonNumericValue(data[varname]) + ';';
+        }
+    }
+
+    let parttexts = [];
+    for (let i = 0; i < numberOfParts; i++) {
+        parttexts[i] = fetchTextFromEditor(`id_subqtext_${i}`);
+    }
+
+    fetchMany([{
+        methodname: 'qtype_formulas_render_question_text',
+        args: {
+            questiontext: fetchTextFromEditor('id_questiontext'),
+            parttexts: parttexts,
+            globalvars: questionvars,
+            partvars: partvars
+        },
+        done: showRenderedQuestionAndParts,
+        fail: Notification.exception
+    }]);
+};
+
+/**
+ * This function is called after the AJAX request to the backend is completed. It will inject
+ * the rendered texts into the preview div.
+ *
+ * @param {object} data rendered version of question text and parts' text
+ */
+const showRenderedQuestionAndParts = (data) => {
+    let div = document.getElementById('qtextpreview_display');
+    div.innerHTML = data.question;
+    for (let text of data.parts) {
+        div.innerHTML += text;
+    }
+};
+
+/**
+ * Derive the column description from the instantiated variables.
+ *
+ * @param {object} data instantiation data as received from the backend
+ * @returns {object} column description object for Tabulator.js
+ */
+const prepareTableColumns = (data) => {
+    let firstRow = data.data[0];
+    // Random variables come first
+    let randomColumns = [];
+    for (let column of firstRow.randomvars) {
+        randomColumns.push({title: column.name, field: `random_${column.name}`, bottomCalc: 'myavg'});
+    }
+
+    // Then we take the global variables
+    let globalColumns = [];
+    for (let column of firstRow.globalvars) {
+        globalColumns.push({title: column.name, field: `global_${column.name}`, bottomCalc: 'myavg'});
+    }
+
+    // Finally, we prepare the groups for each part
+    let partColumns = [];
+    let partIndex = 0;
+    for (let part of firstRow.parts) {
+        let thisPartsColumns = [];
+        for (let vars of part) {
+            thisPartsColumns.push({title: vars.name, field: `part_${partIndex}_${vars.name}`, bottomCalc: 'myavg'});
+        }
+        partColumns.push({title: `part ${partIndex + 1}`, columns: thisPartsColumns});
+        partIndex++;
+    }
+
+    // Now, put it all together
+    let columnDescription = [
+        {title: '#', field: 'id'},
+        {title: 'random variables', columns: randomColumns},
+        {title: 'global variables', columns: globalColumns},
+        ...partColumns
+    ];
+    Tabulator.findTable("#varsdata_display")[0].setColumns(columnDescription);
+    fillTable(data);
+    return columnDescription;
+};
+
+/**
+ * Prepare the data and send it to the Tabulator.js table for display.
+ *
+ * @param {object} data instantiation data as received from the backend
+ */
+const fillTable = (data) => {
+    let allRows = [];
+    let rowCounter = 0;
+    for (let row of data.data) {
+        let thisRow = {id: ++rowCounter};
+        for (let thisVar of row.randomvars) {
+            thisRow[`random_${thisVar.name}`] = thisVar.value;
+        }
+        for (let thisVar of row.globalvars) {
+            thisRow[`global_${thisVar.name}`] = thisVar.value;
+        }
+        let partCounter = 0;
+        for (let thisPart of row.parts) {
+            for (let thisVar of thisPart) {
+                thisRow[`part_${partCounter}_${thisVar.name}`] = thisVar.value;
+            }
+            partCounter++;
+        }
+        allRows.push(thisRow);
+    }
+
+    Tabulator.findTable("#varsdata_display")[0].setData(allRows);
+};
+
+/**
+ * Send the definition of random variables, global variables and parts' local variables
+ * to the backend for instantiation. This will generate a certain number of rows, based
+ * on the number the user has selected in the corresponding dropdown field. Once the
+ * AJAX requeset is completed, the data will be forwarded to {@link prepareTableColumns}.
+ */
+const instantiate = () => {
+    let howMany = document.getElementById('id_numdataset').value;
+    fetchMany([{
+        methodname: 'qtype_formulas_instantiate',
+        args: {
+            n: howMany,
+            randomvars: document.getElementById('id_varsrandom').value,
+            globalvars: document.getElementById('id_varsglobal').value,
+            localvars: [
+                document.getElementById('id_vars1_0').value,
+                document.getElementById('id_vars1_1').value,
+                document.getElementById('id_vars1_2').value,
+            ],
+            answers: [
+                document.getElementById('id_answer_0').value,
+                document.getElementById('id_answer_1').value,
+                document.getElementById('id_answer_2').value,
+            ]
+        },
+        done: prepareTableColumns,
+        fail: Notification.exception
+    }]);
+};
+
+/**
+ * Event handler for the global variables definition. The function will send the text to
+ * the backend and try to evaluate it (together with the random variables, because global
+ * variables can be based on random variables). If there is an error, it will be shown
+ * in the form via {@link showOrClearValidationError}.
+ *
+ * @param {Event} evt Event object
+ */
+const validateGlobalvars = (evt) => {
+    fetchMany([{
+        methodname: 'qtype_formulas_check_random_global_vars',
+        args: {
+            randomvars: document.getElementById('id_varsrandom').value,
+            globalvars: evt.target.value
+        },
+        done: (answer) => {
+            showOrClearValidationError(evt.target.id, answer);
+        },
+        fail: Notification.exception
+    }]);
+};
+
+/**
+ * Event handler for the random variables definition. The function will send the text to
+ * the backend which tries to parse it and instantiate the variables. If there is an error,
+ * it will be shown in the form via {@link showOrClearValidationError}.
+ *
+ * @param {Event} evt Event object
+ */
+const validateRandomvars = (evt) => {
+    fetchMany([{
+        methodname: 'qtype_formulas_check_random_global_vars',
+        args: {
+            randomvars: evt.target.value
+        },
+        done: (answer) => {
+            showOrClearValidationError(evt.target.id, answer);
+        },
+        fail: Notification.exception
+    }]);
+};
+
+/**
+ * Show a validation error below the corresponding form field and set the field
+ * as invalid. Or remove message and marking, if there is no error anymore.
+ *
+ * @param {string} fieldID id of the form field to which the error belongs
+ * @param {string} message error message or empty string, if error is to be removed
+ */
+const showOrClearValidationError = (fieldID, message) => {
+    let field = document.getElementById(fieldID);
+    let annotation = document.getElementById(fieldID.replace(/^id_(.*)$/, 'id_error_$1'));
+    if (message === '') {
+        annotation.innerText = '';
+        field.classList.remove('is-invalid');
+    } else {
+        annotation.innerText = message;
+        field.classList.add('is-invalid');
+        field.focus();
     }
 };
 
