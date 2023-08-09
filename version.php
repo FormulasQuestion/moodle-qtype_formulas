b8a8b6c4b70442deec02d4c715a13150ec12fde2
Philipp Imhof <52650214+PhilippImhof@users.noreply.github.com>
2023-08-09 17:10:31 +0200

    fix typo

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_formulas';
-$plugin->version = 20230809;
+$plugin->version = 2023080900;
 
 $plugin->cron = 0;
 $plugin->requires = 2017111300;
