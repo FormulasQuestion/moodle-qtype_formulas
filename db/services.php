f3f7cb2751f22232d274d74ec03e4bfacad4dfdf
Philipp Imhof <52650214+PhilippImhof@users.noreply.github.com>
2023-12-20 12:04:29 +0100

    cleanup, codestyle

--- db/services.php
+++ db/services.php
@@ -14,43 +14,51 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * External functions and service definitions for the Formulas question type plugin.
+ *
+ * @package    qtype_formulas
+ * @copyright  2023 Philipp Imhof
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 defined('MOODLE_INTERNAL') || die();
 
-$functions = array(
-    'qtype_formulas_instantiate' => array(
-            'classname'   => 'qtype_formulas\external\instantiation',
-            'methodname'  => 'instantiate',
+$functions = [
+    'qtype_formulas_instantiate' => [
+            'classname' => 'qtype_formulas\external\instantiation',
+            'methodname' => 'instantiate',
             'description' => 'Instantiate a Formulas question based on the values provided in the request',
-            'type'        => 'read',
-            'ajax'        => true,
-            'capabilities'  => '',
-            'services' => array()
-    ),
-    'qtype_formulas_check_random_global_vars' => array(
-        'classname'   => 'qtype_formulas\external\instantiation',
-        'methodname'  => 'check_random_global_vars',
+            'type' => 'read',
+            'ajax' => true,
+            'capabilities' => '',
+            'services' => []
+    ],
+    'qtype_formulas_check_random_global_vars' => [
+        'classname' => 'qtype_formulas\external\instantiation',
+        'methodname' => 'check_random_global_vars',
         'description' => 'Validate definition of random and/or global variables',
-        'type'        => 'read',
-        'ajax'        => true,
-        'capabilities'  => '',
-        'services' => array()
-    ),
-    'qtype_formulas_check_local_vars' => array(
-        'classname'   => 'qtype_formulas\external\instantiation',
-        'methodname'  => 'check_local_vars',
+        'type' => 'read',
+        'ajax' => true,
+        'capabilities' => '',
+        'services' => []
+    ],
+    'qtype_formulas_check_local_vars' => [
+        'classname' => 'qtype_formulas\external\instantiation',
+        'methodname' => 'check_local_vars',
         'description' => 'Validate definition of a part\'s local variables',
-        'type'        => 'read',
-        'ajax'        => true,
-        'capabilities'  => '',
-        'services' => array()
-    ),
-    'qtype_formulas_render_question_text' => array(
-        'classname'   => 'qtype_formulas\external\instantiation',
-        'methodname'  => 'render_question_text',
+        'type' => 'read',
+        'ajax' => true,
+        'capabilities' => '',
+        'services' => []
+    ],
+    'qtype_formulas_render_question_text' => [
+        'classname' => 'qtype_formulas\external\instantiation',
+        'methodname' => 'render_question_text',
         'description' => 'Substitute variables inside the question and/or part by their values',
-        'type'        => 'read',
-        'ajax'        => true,
-        'capabilities'  => '',
-        'services' => array()
-    )
-);
+        'type' => 'read',
+        'ajax' => true,
+        'capabilities' => '',
+        'services' => []
+    ]
+];
