ca3c80f955485ba3b4404dfb95060a2dd43dafcf
Philipp Imhof <52650214+PhilippImhof@users.noreply.github.com>
2025-06-17 07:50:13 +0200

    first steps to allow formatting of input boxes

--- question.php
+++ question.php
@@ -1035,7 +1035,7 @@ class qtype_formulas_part {
      */
     public static function scan_for_answer_boxes(string $text): array {
         // Match the text and store the matches.
-        preg_match_all('/\{(_u|_\d+)(:(_[A-Za-z]|[A-Za-z]\w*)(:(MCE|MCS|MCES))?)?\}/', $text, $matches);
+        preg_match_all('/\{(_u|_\d+)(:(_[A-Za-z]|[A-Za-z]\w*)(:(MCE|MCS|MCES))?)?((\|[\w =#]*)*)\}/', $text, $matches);
 
         $boxes = [];
 
@@ -1057,9 +1057,31 @@ class qtype_formulas_part {
                 'options' => $matches[3][$i],
                 'dropdown' => (substr($matches[4][$i], 0, 4) === ':MCE'),
                 'shuffle' => (substr($matches[4][$i], -1) === 'S'),
+                'format' => self::parse_box_formatting_options(substr($matches[6][$i], 1)),
             ];
         }
         return $boxes;
+    }
+
+    /**
+     * FIXME
+     *
+     * @param string $settings
+     * @return array
+     */
+    protected static function parse_box_formatting_options(string $settings): array {
+        $options = explode('|', $settings);
+
+        $result = [];
+        foreach ($options as $option) {
+            if (strstr($option, '=') === false) {
+                continue;
+            }
+            $namevalue = explode('=', $option);
+            $result[$namevalue[0]] = $namevalue[1];
+        }
+
+        return $result;
     }
 
     /**
